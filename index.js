899bba40acf047f6cffe81c1850b798bc6fcc984
Wasan Chubuatong <wasanchubuatong@gmail.com>
2025-06-16 21:17:17 +0800

    Add logs-service to generate warn & error report

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const { processLogs } = require("./service/logs-service");
 
 const inputFile = path.join("input", "logs.log");
 const fullInputPath = path.join(process.cwd(), inputFile);
@@ -8,5 +9,10 @@ if (!fs.existsSync(fullInputPath)) {
     console.error(`File not found: ${fullInputPath}`);
 }
 
-const logLines = fs.readFileSync(fullInputPath, "utf-8").trim().split("\n");
-console.log(logLines);
+const reportDir = processLogs(inputFile);
+
+if (reportDir) {
+  console.log(`Logs written to: ${reportDir}`);
+} else {
+  console.log("No log files created (No warnings or errors)");
+}
